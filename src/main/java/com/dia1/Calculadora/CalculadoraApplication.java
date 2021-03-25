c8461a5cd8f5726cfa95a4753fea440873dfcd7a
Juan Ruiz <esteban.ruiz@utp.edu.co>
2021-03-25 12:41:03 -0500

    Inicialización

---
+++ src/main/java/com/dia1/Calculadora/CalculadoraApplication.java
@@ -0,0 +1,35 @@
+package com.dia1.Calculadora;
+
+import java.io.PrintStream;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@RequestMapping("calc")
+
+public class CalculadoraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculadoraApplication.class, args);
+	}
+        
+        @GetMapping("/{num1}/{num2}")
+        public String nums( 
+                @PathVariable("num1") int num1, 
+                @PathVariable("num2") int num2){
+            return "Números: " + num1 + " " + num2;
+        }
+        
+         @GetMapping()
+        public String nums2( 
+                @RequestParam ("num1") int num1, 
+                @RequestParam ("num2") int num2){
+            return "Números: " + num1 + " " + num2;
+        }
+}
---
+++ src/test/java/com/dia1/Calculadora/CalculadoraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dia1.Calculadora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculadoraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
